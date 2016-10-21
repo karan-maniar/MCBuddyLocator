6c14c2407dd1bdf183c21bd1ba3c8db57e0f1510
Karan Maniar <karan.maniar@hotmail.com>
2016-10-21 14:35:05 -0700

    Testing

--- app/src/main/java/mcproject/buddylocator/MainActivity.java
+++ app/src/main/java/mcproject/buddylocator/MainActivity.java
@@ -14,6 +14,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void test()
     {
-        String forTest = "Just for testing";
+        String forTesting = "Just for testing";
     }
 }
